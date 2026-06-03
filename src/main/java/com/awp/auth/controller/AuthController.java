9944dbf8d1f73aef53d65bad80d1758cf6ea89cd
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-06-03 22:44:44 +0530

    feat : postgres and mysql databases configured.

--- src/main/java/com/awp/auth/controller/AuthController.java
+++ src/main/java/com/awp/auth/controller/AuthController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -20,6 +17,7 @@ import java.net.URI;
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("http://localhost:5173/")
 public class AuthController {
 
     private final UserAuthService userAuthService;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 --MySQL
-INSERT IGNORE INTO roles (id, name) VALUES (1, 'USER');
-INSERT IGNORE INTO roles (id, name) VALUES (2, 'ADMIN');
+--INSERT IGNORE INTO roles (id, name) VALUES (1, 'USER');
+--INSERT IGNORE INTO roles (id, name) VALUES (2, 'ADMIN');
 
 --postgres
---INSERT INTO roles (id, name) VALUES (1, 'USER') ON CONFLICT (id) DO NOTHING;
---INSERT INTO roles (id, name) VALUES (2, 'ADMIN') ON CONFLICT (id) DO NOTHING;
+INSERT INTO roles (id, name) VALUES (1, 'USER') ON CONFLICT (id) DO NOTHING;
+INSERT INTO roles (id, name) VALUES (2, 'ADMIN') ON CONFLICT (id) DO NOTHING;
