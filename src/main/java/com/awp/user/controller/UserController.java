0d57e98ea5aabd40e19bd60fb5758e5c170418c6
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-06-06 17:12:04 +0530

    feat :timestamp format updated

--- src/main/java/com/awp/user/controller/UserController.java
+++ src/main/java/com/awp/user/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -26,7 +26,6 @@ public class UserController {
             @RequestParam ( defaultValue = AppConstants.DEFAULT_PAGE_NO) int pageNo,
             @RequestParam ( defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int pageSize,
             @RequestParam ( defaultValue = AppConstants.DEFAULT_SORT_BY) String sortBy
-
     ) {
 
         return ResponseEntity.ok(userService.getAllUsers(pageNo,pageSize,sortBy));
